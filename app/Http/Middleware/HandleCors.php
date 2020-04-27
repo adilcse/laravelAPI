8b0f99a3ffb865967db666c839fe8f2a71525846
adilcse <adil.cs.work@gmail.com>
2020-04-27 14:13:17 +0530

    dead code removed

--- app/Http/Middleware/HandleCors.php
+++ app/Http/Middleware/HandleCors.php
@@ -15,12 +15,6 @@ class HandleCors
      */
     public function handle($request, Closure $next)
     {
-        // $output = $next($request);
-        // $response=$output
-        //             ->header('Access-Control-Allow-Origin','*')
-        //             ->header('Access-Control-Allow-Headers','api_token,X-Requested-With, Cache-Control, Content-Type, Authorization, Accept-Language,Content-Language,Last-Event-ID,X-HTTP-Method-Override')
-        //             ->header('Access-Control-Allow-Methods','GET, POST, PUT, DELETE, OPTIONS')
-        //             ->header('Access-Control-Allow-Credentials', true);
         $headers = [
             'Access-Control-Allow-Origin'      => '*',
             'Access-Control-Allow-Methods'     => 'POST, GET, OPTIONS, PUT, DELETE',
